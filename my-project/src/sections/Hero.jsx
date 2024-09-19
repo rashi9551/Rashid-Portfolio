97b0143160eb8cd685c4fbc3ae7bf5662adabebe
mubasir umbi <mubasirumbi@gmail.com>
2024-09-19 15:36:53 +0530

    first commit

---
+++ my-project/src/sections/Hero.jsx
@@ -0,0 +1,95 @@
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { PerspectiveCamera } from "@react-three/drei";
+import HackerRoom from "../components/HackerRoom";
+import CanvasLoader from "../components/CanvasLoader";
+import { Leva, useControls } from "leva";
+import { useMediaQuery } from "react-responsive";
+import { calculateSizes } from "../constants";
+import Target from "../components/Target";
+import ReactLogo from "../components/ReactLogo";
+
+const Hero = () => {
+  const isMobile = useMediaQuery({maxWidth: 768})
+  const isSmall = useMediaQuery({maxWidth: 440})
+  const isTab = useMediaQuery({minWidth: 768, maxWidth: 1024})
+
+  const size = calculateSizes(isSmall, isMobile,isTab)
+
+  // const x = useControls("HackerRoom", {
+  //   positionX: {
+  //     value: 2.5,
+  //     min: -10,
+  //     max: 10,
+  //   },
+  //   positionY: {
+  //     value: 2.5,
+  //     min: -10,
+  //     max: 10,
+  //   },
+  //   positionZ: {
+  //     value: 2.5,
+  //     min: -10,
+  //     max: 10,
+  //   },
+
+  //   rotationX: {
+  //     value: 2.5,
+  //     min: -10,
+  //     max: 10,
+  //   },
+  //   rotationY: {
+  //     value: 2.5,
+  //     min: -10,
+  //     max: 10,
+  //   },
+  //   rotationZ: {
+  //     value: 2.5,
+  //     min: -10,
+  //     max: 10,
+  //   },
+  //   scale: {
+  //     value: 1,
+  //     min: 0.1,
+  //     max: 10,
+  //   },
+  // });
+
+  return (
+    <section className="min-h-screen w-full flex flex-col relative">
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+        <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
+          Hi, I am Mubasir <span className="waving-hand">👋</span>
+        </p>
+        <p className="hero_tag text-gray_gradient">
+          Building Products & Brands
+        </p>
+      </div>
+
+      <div className="w-full h-full absolute inset-0">
+        <Leva />
+        <Canvas className="w-full h-full">
+          <Suspense fallback={CanvasLoader}>
+            <PerspectiveCamera makeDefault position={[0, 0, 20]} />
+            <HackerRoom
+              scale={size.deskScale}
+              position={size.deskPosition}
+              rotation={[0, -Math.PI / 2, 0]}
+              // scale={x.scale}
+              // position={[x.positionX, x.positionY, x.positionZ]}
+              // rotation={[x.rotationX, x.rotationY, x.rotationZ]}
+            />
+            <group>
+              <Target position={size.targetPosition}/>
+              <ReactLogo position={size.reactLogoPosition} />
+            </group>
+            <ambientLight intensity={1} />
+            <directionalLight position={[10, 10, 10]} intensity={0.5} />
+          </Suspense>
+        </Canvas>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;

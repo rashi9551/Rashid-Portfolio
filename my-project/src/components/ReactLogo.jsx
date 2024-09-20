f98c0c869e442cb6c78080a7bae63288db613c85
mubasir umbi <mubasirumbi@gmail.com>
2024-09-20 19:52:08 +0530

    contact section completed

--- my-project/src/components/ReactLogo.jsx
+++ my-project/src/components/ReactLogo.jsx
@@ -1,23 +1,24 @@
-import React  from 'react'
-import { Float, useGLTF } from '@react-three/drei'
+
+import { Float, useGLTF } from '@react-three/drei';
 
 const ReactLogo = (props) => {
-  const { nodes, materials } = useGLTF('/models/react.glb')
+  const { nodes, materials } = useGLTF('models/react.glb');
+
   return (
     <Float floatIntensity={1}>
-      <group {...props} scale={0.3} position={[8, 8, 0]}>
+      <group position={[8, 8, 0]} scale={0.3} {...props} dispose={null}>
         <mesh
           geometry={nodes['React-Logo_Material002_0'].geometry}
           material={materials['Material.002']}
-          position={[0, 0.79, 0.181]}
+          position={[0, 0.079, 0.181]}
           rotation={[0, 0, -Math.PI / 2]}
-          scale={[0.39, 0.39, 0.5]}
+          scale={[0.392, 0.392, 0.527]}
         />
       </group>
     </Float>
-  )
-}
+  );
+};
 
-useGLTF.preload('/models/react.glb')
+useGLTF.preload('models/react.glb');
 
-export default ReactLogo
+export default ReactLogo;
--- my-project/src/sections/Hero.jsx
+++ my-project/src/sections/Hero.jsx
@@ -1,101 +1,143 @@
-import React, { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
-import { PerspectiveCamera } from "@react-three/drei";
-import HackerRoom from "../components/HackerRoom";
-import CanvasLoader from "../components/CanvasLoader";
-import { Leva, useControls } from "leva";
-import { useMediaQuery } from "react-responsive";
-import { calculateSizes } from "../constants";
-import Target from "../components/Target";
-import ReactLogo from "../components/ReactLogo";
-import Cube from "../components/Cube";
-import Rings from "../components/Ring";
-import HeroCamera from "../components/HeroCamera";
-import Button from "../components/Button";
+// import { Leva } from 'leva';
+// import { Suspense } from 'react';
+// import { Canvas } from '@react-three/fiber';
+// import { useMediaQuery } from 'react-responsive';
+// import { PerspectiveCamera } from '@react-three/drei';
+
+// import Cube from '../components/Cube.jsx';
+// import Rings from '../components/Ring.jsx';
+// import ReactLogo from '../components/ReactLogo.jsx';
+// import Button from '../components/Button.jsx';
+// import Target from '../components/Target.jsx';
+// import CanvasLoader from '../components/CanvasLoader.jsx';
+// import HeroCamera from '../components/HeroCamera.jsx';
+// import { calculateSizes } from '../constants/index.js';
+// import { HackerRoom } from '../components/HackerRoom.jsx';
+
+// const Hero = () => {
+//   // Use media queries to determine screen size
+//   const isSmall = useMediaQuery({ maxWidth: 440 });
+//   const isMobile = useMediaQuery({ maxWidth: 768 });
+//   const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
+
+//   const sizes = calculateSizes(isSmall, isMobile, isTablet);
+
+//   return (
+//     <section className="min-h-screen w-full flex" id="home">
+//       <div className="w-1/2 flex flex-col mt-20 c-space gap-3  justify-center items-center">
+//         <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
+//           Hi, I am Mubasir <span className="waving-hand">👋</span>
+//         </p>
+//         <p className="hero_tag text-gray_gradient">Building Products & Brands</p>
+//         <a href="#about" className="w-fit">
+//           <Button name="Let's work together" isBeam containerClass="sm:w-fit w-full sm:min-w-96" />
+//         </a>
+//         <div className="flex gap-3">
+//           <div className="social-icon">
+//             <img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2" />
+//           </div>
+//           <div className="social-icon">
+//             <img src="/assets/twitter.svg" alt="twitter" className="w-1/2 h-1/2" />
+//           </div>
+//           <div className="social-icon">
+//             <img src="/assets/instagram.svg" alt="instagram" className="w-1/2 h-1/2" />
+//           </div>
+//         </div>
+  
+//       </div>
+
+//       <div className="w-1/2 flex justify-center items-center" >
+//         <Canvas className="w-full h-full pb-40">
+//           <Suspense fallback={<CanvasLoader />}>
+//             {/* To hide controller */}
+//             <Leva hidden />
+//             <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+
+//             <HeroCamera isMobile={isMobile}>
+//               <HackerRoom scale={sizes.deskScale} position={sizes.deskPosition} rotation={[0.1, -Math.PI, 0]} />
+//             </HeroCamera>
+
+//             <group>
+//               {/* <Target position={sizes.targetPosition} />
+//               <ReactLogo position={sizes.reactLogoPosition} /> */}
+//               {/* <Rings position={sizes.ringPosition} /> */}
+//               {/* <Cube position={sizes.cubePosition} /> */}
+//             </group>
+
+//             <ambientLight intensity={1} />
+//             <directionalLight position={[10, 10, 10]} intensity={0.5} />
+//           </Suspense>
+//         </Canvas>
+//       </div>
+
+//       {/* <div className="absolute bottom-7 left-0 right-0 w-full z-10 c-space">
+//         <a href="#about" className="w-fit">
+//           <Button name="Let's work together" isBeam containerClass="sm:w-fit w-full sm:min-w-96" />
+//         </a>
+//       </div> */}
+//     </section>
+//   );
+// };
+
+// export default Hero;
+
+import { Leva } from 'leva';
+import { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { useMediaQuery } from 'react-responsive';
+import { PerspectiveCamera } from '@react-three/drei';
+
+import Cube from '../components/Cube.jsx';
+import Rings from '../components/Ring.jsx';
+import ReactLogo from '../components/ReactLogo.jsx';
+import Button from '../components/Button.jsx';
+import Target from '../components/Target.jsx';
+import CanvasLoader from '../components/CanvasLoader.jsx';
+import HeroCamera from '../components/HeroCamera.jsx';
+import { calculateSizes } from '../constants/index.js';
+import { HackerRoom } from '../components/HackerRoom.jsx';
 
 const Hero = () => {
-  const isMobile = useMediaQuery({ maxWidth: 768 });
+  // Use media queries to determine screen size
   const isSmall = useMediaQuery({ maxWidth: 440 });
-  const isTab = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
 
-  const size = calculateSizes(isSmall, isMobile, isTab);
-
-  // const x = useControls("HackerRoom", {
-  //   positionX: {
-  //     value: 2.5,
-  //     min: -10,
-  //     max: 10,
-  //   },
-  //   positionY: {
-  //     value: 2.5,
-  //     min: -10,
-  //     max: 10,
-  //   },
-  //   positionZ: {
-  //     value: 2.5,
-  //     min: -10,
-  //     max: 10,
-  //   },
-
-  //   rotationX: {
-  //     value: 2.5,
-  //     min: -10,
-  //     max: 10,
-  //   },
-  //   rotationY: {
-  //     value: 2.5,
-  //     min: -10,
-  //     max: 10,
-  //   },
-  //   rotationZ: {
-  //     value: 2.5,
-  //     min: -10,
-  //     max: 10,
-  //   },
-  //   scale: {
-  //     value: 1,
-  //     min: 0.1,
-  //     max: 10,
-  //   },
-  // });
+  const sizes = calculateSizes(isSmall, isMobile, isTablet);
 
   return (
-    <section className="min-h-screen w-full flex flex-col relative">
-      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
-        <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
-          Hi, I am Mubasir <span className="waving-hand">👋</span>
+    <section className="min-h-screen w-full flex flex-col relative" id="home">
+      <div className="w-full mx-auto flex flex-col sm:mt-24 mt-10 c-space gap-3 ">
+        <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
+          Hi, I am Adrian <span className="waving-hand">👋</span>
         </p>
-        <p className="hero_tag text-gray_gradient">
-          Building Products & Brands
-        </p>
+        <p className="hero_tag text-gray_gradient">Building Products & Brands</p>
       </div>
 
-      <div className="w-full h-full absolute inset-0">
-        <Leva />
+      <div className="w-full h-full absolute inset-0 mt-12">
         <Canvas className="w-full h-full">
-          <Suspense fallback={CanvasLoader}>
-            <PerspectiveCamera makeDefault position={[0, 0, 20]} />
+          <Suspense fallback={<CanvasLoader />}>
+            {/* To hide controller */}
+            <Leva hidden />
+            <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+
             <HeroCamera isMobile={isMobile}>
-              <HackerRoom
-                scale={size.deskScale}
-                position={size.deskPosition}
-                rotation={[0, -Math.PI / 2, 0]}
-                // scale={x.scale}
-                // position={[x.positionX, x.positionY, x.positionZ]}
-                // rotation={[x.rotationX, x.rotationY, x.rotationZ]}
-              />
+              <HackerRoom scale={sizes.deskScale} position={sizes.deskPosition} rotation={[0.1, -Math.PI, 0]} />
             </HeroCamera>
+
             <group>
-              <Target position={size.targetPosition} />
-              <ReactLogo position={size.reactLogoPosition} />
-              <Cube position={size.cubePosition} />
-              <Rings position={size.ringPosition} />
+              <Target position={sizes.targetPosition} />
+              <ReactLogo position={sizes.reactLogoPosition} />
+              <Rings position={sizes.ringPosition} />
+              <Cube position={sizes.cubePosition} />
             </group>
+
             <ambientLight intensity={1} />
             <directionalLight position={[10, 10, 10]} intensity={0.5} />
           </Suspense>
         </Canvas>
       </div>
+
       <div className="absolute bottom-7 left-0 right-0 w-full z-10 c-space">
         <a href="#about" className="w-fit">
           <Button name="Let's work together" isBeam containerClass="sm:w-fit w-full sm:min-w-96" />

8e22538392513a13d2f834eff0a311f775b86911
mubasir umbi <mubasirumbi@gmail.com>
2024-12-03 19:50:46 +0530

    global margit top aded

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,28 @@
-import React from 'react'
-import Navbar from './sections/Navbar'
-import Hero from './sections/Hero'
-import About from './sections/About'
-import Projects from './sections/Projects'
-import Clients from './sections/Clients'
-import Contact from './sections/Contact'
-import Footer from './sections/Footer'
-import WorkExperience from './sections/WorkExperience'
-import InfiniteMovingCards from './components/ui/InfiniteMovingCard'
-import WhatsappChat from './components/WhatsappChat'
-
+import React from "react";
+import Navbar from "./sections/Navbar";
+import Hero from "./sections/Hero";
+import About from "./sections/About";
+import Projects from "./sections/Projects";
+import Contact from "./sections/Contact";
+import Footer from "./sections/Footer";
+import WorkExperience from "./sections/WorkExperience";
+import InfiniteMovingCards from "./components/ui/InfiniteMovingCard";
+import WhatsappChat from "./components/WhatsappChat";
 
 const App = () => {
   return (
-    <main className='max-w-7xl mx-auto'>
+    <main className="max-w-full mx-16 mt-5 md:mt-16">
       <WhatsappChat />
       <Navbar />
       <Hero />
       <About />
       <Projects />
       <WorkExperience />
-      <InfiniteMovingCards/>
-      {/* <Clients /> */}
+      <InfiniteMovingCards />
       <Contact />
       <Footer />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
